a3d470a782562450582f5adc41d5392f6cda963d
Nickman282 <manturov.nik@gmail.com>
2026-02-25 15:15:19 +0000

    Johnny Replicas + Extras

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Pisets Bot
-# Application ID: 1475883749722816727
-# Public Key: d7b771cfed8684b2a709762a340ae4e1ab20fa918f47eb3229f98f1af6c81457
+# Johnny Bot
 
 import random
 import discord
@@ -11,7 +9,7 @@ from datetime import datetime
 from discord.ext import tasks
 from dotenv import load_dotenv
 
-from src.cog_replica import Replicas
+from src.cog_replica import Replicas, bot_callable
 
 # Load tokens
 
@@ -24,43 +22,20 @@ if not isinstance(CHANNEL_ID, int):
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-
-
-# Create Client class instance
+# Create Bot class instance, initialize on_ready coroutine, run
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.ext.commands.Bot(command_prefix = '', intents=intents)
-
+client = discord.ext.commands.Bot(command_prefix = bot_callable, intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+
     # Add a replica Cog
     replica = Replicas(client, CHANNEL_ID)
     await client.add_cog(replica)
-    #custom_event.start()
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-'''
-@tasks.loop(seconds=5)
-async def custom_event():
-    # Get channel 
-    message = client.get_channel(CHANNEL_ID)
-
-    # Get timestamp
-    ex = 'Tue Feb 24 22:38:00 2026'
-    curr_time = time.time()
-    if curr_time > time.mktime(time.strptime(ex)):
-        await message.send(f'@everyone The time is {time.ctime(curr_time)}')
-'''
 
 client.run(BOT_TOKEN)
 
--- src/cog_replica.py
+++ src/cog_replica.py
@@ -4,12 +4,33 @@ import asyncio
 import time
 import os
 import re 
+import random
 from datetime import datetime
 from discord.ext import tasks, commands
 
-call_pattern = "писе[ц|тс]|pisets"
+#call_pattern = "писе[ц|тс]|pisets"
 
-#def bot_callable(bot, )
+# Define a call pattern for the bot (case-insensitive)
+call_pattern = ".*жонни|johnny"
+user_pattern = "#User#"
+specuser_pattern = "#SpecUser#"
+
+# Load in replicas
+path = f"{os.getcwd()}/Data/johnny_quotes.txt"
+list_of_replicas = []
+with open(path, 'r') as f:
+    for line in f:
+        list_of_replicas.append(line)
+
+# Callabale function used to define if bot should respond to message
+def bot_callable(bot, message):
+    msg_content = message.content
+    matches = re.findall(call_pattern, msg_content, re.IGNORECASE)
+    if matches != []:
+        command_prefix = f'{msg_content.split(' ')[0]}'
+    else:
+        command_prefix = call_pattern
+    return command_prefix
 
 class Replicas(commands.Cog):
 
@@ -24,13 +45,28 @@ class Replicas(commands.Cog):
             except:
                 raise TypeError("Only integers are allowed") 
         
-
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
-        else:
+        elif message.channel.id == self.channel:
             msg_content = message.content
             matches = re.findall(call_pattern, msg_content, re.IGNORECASE)
             if matches != []:
-                await message.channel.send('Some replica')
+                # Select a random replica
+                rand_index = random.randint(0, len(list_of_replicas)-1)
+                replica = list_of_replicas[rand_index]
+
+                # Check if quote contains user_pattern
+                user_matches = re.findall(user_pattern, replica, re.IGNORECASE)
+                if user_matches != []:
+                    author = message.author.id # Get "Member" instance, then the name string from it
+                    replica = replica.replace(user_pattern, f'<@{author}>')
+
+                # Check if quote contains specuser_pattern
+                specuser_matches = re.findall(specuser_pattern, replica, re.IGNORECASE)
+                if specuser_matches != []:
+                    spec_author = message.author.guild.owner_id # Get "Member" instance, then "Guild", then its owner
+                    replica = replica.replace(specuser_pattern, f'<@{spec_author}>')
+                
+                await message.channel.send(replica)
